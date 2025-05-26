01a6c32ac9386ae3dd1a08cbb0e49c21f672ad80
Macattack1125 <macraw50@kingshigh.school.nz>
2025-05-27 09:59:32 +1200

    Added map and break wall enemy kind of

---
+++ addons/PlayerCharacter/WallDestroyer.gd
@@ -0,0 +1,7 @@
+extends RayCast3D
+
+func _physics_process(delta: float) -> void:
+	if is_colliding():
+		var collider = get_collider()
+		if collider.get_parent().get_parent().is_in_group("Mazea"):
+			collider.get_parent().queue_free()
