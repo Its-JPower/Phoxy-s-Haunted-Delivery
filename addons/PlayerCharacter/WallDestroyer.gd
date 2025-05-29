6f9045ce977d0c5345588c42c8b71167c16b1f26
Macattack1125 <macraw50@kingshigh.school.nz>
2025-05-29 12:18:49 +1200

    added shooting

--- addons/PlayerCharacter/WallDestroyer.gd
+++ addons/PlayerCharacter/WallDestroyer.gd
@@ -3,5 +3,5 @@ extends RayCast3D
 func _physics_process(delta: float) -> void:
 	if is_colliding():
 		var collider = get_collider()
-		if collider.get_parent().get_parent().is_in_group("Mazea"):
+		if collider.get_parent().get_parent().is_in_group("Maze"):
 			collider.get_parent().queue_free()
