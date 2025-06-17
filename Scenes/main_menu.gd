8ce34c0958d564ea369d7885fb9a9bc6c47fba3d
ItsJPower <joseva15@kingshigh.school.nz>
2025-06-17 12:25:36 +1200

    main menu 2

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,11 +1,18 @@
 extends CanvasLayer
 
-@onready var foxy_anim: AnimationPlayer = $Foxy/AnimationPlayer
+@onready var foxy_anim: AnimationPlayer = $Background/Foxy/AnimationPlayer
+@onready var camera: Camera3D = $Background/SubViewportContainer/SubViewport/Camera3D
+@onready var tween = $Tween
+
+var left_rotation = Vector3(0, deg_to_rad(-30), 0)
+var right_rotation = Vector3(0, deg_to_rad(30), 0)
+var going_right = true
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	foxy_anim.play("idle", .25, 0.25)
+	pan_camera()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,3 +30,9 @@ func _on_options_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit(1)
+
+func pan_camera():
+	var target_rotation = right_rotation if going_right else left_rotation
+	tween.tween_property(camera, "rotation", target_rotation, 10.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	going_right = !going_right
+	tween.finished.connect(pan_camera, CONNECT_ONE_SHOT)
