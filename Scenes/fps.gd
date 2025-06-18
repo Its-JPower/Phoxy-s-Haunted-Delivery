e095182252d0eda5ff0a07aed6392acc6e532290
ItsJPower <joseva15@kingshigh.school.nz>
2025-06-18 12:51:21 +1200

    Revert "Merge branch 'BasicMainCharacter'"
    This reverts commit 27cdf9f4418af70dd3f98130fe75220b4c343b89, reversing
    changes made to ee718a4f2b195cded92e7d960e03b38dea455187.

--- Scenes/fps.gd
+++
@@ -1,7 +0,0 @@
-extends Node3D
-
-@onready var character: CharacterBody3D = $".."
-@onready var foxy_2: Node3D = $"."
-
-func _physics_process(delta: float) -> void:
-	foxy_2.rotation = character.rotation
