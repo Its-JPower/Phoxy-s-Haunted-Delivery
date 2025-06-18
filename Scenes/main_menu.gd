f95971751829bcc800ae7baa1f58ae95a6e0bb9b
ItsJPower <joseva15@kingshigh.school.nz>
2025-06-18 12:47:13 +1200

    Updated Manu Mein

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,23 +1,21 @@
 extends CanvasLayer
 
 @onready var foxy_anim: AnimationPlayer = $Background/Foxy/AnimationPlayer
-@onready var camera: Camera3D = $Background/SubViewportContainer/SubViewport/Camera3D
-@onready var tween = $Tween
+@onready var camera: Camera3D = $Background/SubViewportContainer/SubViewport/Node3D/Camera3D
+@onready var tween = $Twee
 
-var left_rotation = Vector3(0, deg_to_rad(-30), 0)
-var right_rotation = Vector3(0, deg_to_rad(30), 0)
-var going_right = true
+
+var rotation_speed = .125
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	foxy_anim.play("idle", .25, 0.25)
-	pan_camera()
+	foxy_anim.play("idle", .25, 0.125)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	camera.rotate_y(rotation_speed * delta)
 
 
 
@@ -30,9 +28,3 @@ func _on_options_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit(1)
-
-func pan_camera():
-	var target_rotation = right_rotation if going_right else left_rotation
-	tween.tween_property(camera, "rotation", target_rotation, 10.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-	going_right = !going_right
-	tween.finished.connect(pan_camera, CONNECT_ONE_SHOT)
